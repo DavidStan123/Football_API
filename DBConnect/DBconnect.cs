1fa60a614a80bcf7ec80c0b7201b566abb1b6376
DavidStan123 <168443239+DavidStan123@users.noreply.github.com>
2024-04-30 11:38:01 +0200

    Add files via upload

---
+++ DBConnect/DBconnect.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+
+namespace Champion_League.DBConnector
+{
+    public class DBconnect
+    {
+        
+        string connectionString = "Data Source=(localdb)\\MSSQLLOCALDB;Initial Catalog=LeagueOfChampions;Integrated Security=True";
+
+            public SqlConnection Connect()
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Database connection successful.");
+                    return connection;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error connecting to database: " + ex.Message);
+                    return null;
+                }
+            }
+        
+    }
+
+
+}
